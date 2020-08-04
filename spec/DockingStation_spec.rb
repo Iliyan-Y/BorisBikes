5a3a4c9640aa11008d4114add0afb0c8bbb6a331
Iliyan-Y <maximizebg@gmail.com>
2020-08-04 18:32:27 +0300

    Gets a bike test

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,8 +1,13 @@
 require "Docking_Station"
+require "Bike"
 
 describe DockingStation do
    docking_station = DockingStation.new
+   bike = Bike.new
   it "release a bike" do
     expect(docking_station.release_bike).to eq true
+  end
+  it "gets a bike" do
+    expect(bike.working?).to eq true
   end
 end
